ef13b7e0bf16613a0f34ff4a64437563377655d8
SoulofAO <sgsggssg1@gmail.com>
2023-01-15 21:11:49 +0400

    LastUbdate

--- Task1/MainClasses/Sqlite.py
+++ Task1/MainClasses/Sqlite.py
@@ -1,6 +1,6 @@
 import sqlite3
 from xml.etree.ElementTree import Element, SubElement, tostring
-
+from Subsistems.ResiterSubsistem import NameSubsistem
 class UHotelDataBase:
     def __init__(self):
         self.__Connect = sqlite3.connect('DataBaseHotel.db')
@@ -10,7 +10,8 @@ class UHotelDataBase:
         Family TEXT,
         Second_Name TEXT,
         Passport TEXT,
-        Comment TEXT);
+        Comment TEXT,
+        ID TEXT);
         """)
 
         self.Cursor.execute("""CREATE TABLE IF NOT EXISTS HotelRooms(
@@ -24,7 +25,8 @@ class UHotelDataBase:
          DateOn TEXT, 
          DateOff TEXT,
          Passport TEXT,
-         ID TEXT);
+         HotelRoomID TEXT,
+         ClientID TEXT);
          """)
 
     @property
@@ -49,29 +51,54 @@ class UHotelDataBase:
         self.Cursor.execute('DELETE FROM Bookings;', )
 
         for Client in Hotel.Clients:
-            LocalClient = (Client.Name, Client.Family, Client.Second_Name,Client.passport, Client.Comment)
-            self.Cursor.execute("INSERT INTO Clients VALUES(?, ?, ?, ?, ?);",  LocalClient)
+            LocalClient = (Client.Name, Client.Family, Client.Second_Name, Client.passport, Client.Comment, Client.ID)
+            self.Cursor.execute("INSERT INTO Clients VALUES(?, ?, ?, ?, ?, ?);",  LocalClient)
         for Room in Hotel.HotelRooms:
             LocalHotel = (Room.MaxNumber, Room.Comfort, Room.Pay, Room.ID)
             self.Cursor.execute("INSERT INTO HotelRooms VALUES(?, ?, ?, ?);", LocalHotel)
         for Booking in Hotel.Bookings:
             Passport = Booking.Client.passport
             ID = Booking.HotelRoom.ID
-            LocalBooking=(Booking.DateOn, Booking.DateOff,Passport,ID)
-            self.Cursor.execute("INSERT INTO Bookings VALUES(?, ?, ?, ?);", LocalBooking)
+            ClientID = NameSubsistem.GetIDByReference(Booking.Client)
+            HotelRoomID = NameSubsistem.GetIDByReference(Booking.HotelRoom)
+            LocalBooking=(Booking.DateOn, Booking.DateOff, Passport, ClientID, HotelRoomID)
+            self.Cursor.execute("INSERT INTO Bookings VALUES(?, ?, ?, ?, ?);", LocalBooking)
     def LoadAll(self):
         root = Element('Hotel')
         self.Cursor.execute("SELECT * FROM Clients;")
         Clients = self.Cursor.fetchall()
         for Client in Clients:
-            print(Client[0])
-            X = print(Client[0])
             child = SubElement(root, "Client")
-            child.attrib = {"Name": X, "Family": str(Client[1]), "Second_Name":str(Client[2]),"Passport":str(Client[3]),"Comment":str(Client[4])}
-        file = open('Hotel.xml', 'w')
-        print(tostring(root))
+            child.set("Name", Client[0])
+            child.set("Family", Client[1])
+            child.set("Second_Name", Client[2])
+            child.set("Passport", Client[3])
+            child.set("Comment", Client[4])
+            child.set("ID", Client[5])
+        self.Cursor.execute("SELECT * FROM Bookings;")
+        Bookings = self.Cursor.fetchall()
+        for Booking in Bookings:
+            child = SubElement(root, "Booking")
+            child.set("DateOn", Booking[0])
+            child.set("DateOff", Booking[1])
+            child.set("Passport", Booking[2])
+            child.set("HotelRoomID", Booking[3])
+            child.set("ClientID", Booking[4])
+        self.Cursor.execute("SELECT * FROM HotelRooms;")
+        HotelRooms = self.Cursor.fetchall()
+        for HotelRoom in HotelRooms:
+            child = SubElement(root, "HotelRoom")
+            child.set("MaxNumber",HotelRoom[0])
+            child.set("Comfort", HotelRoom[1])
+            child.set("Pay", HotelRoom[2])
+            child.set("ID", HotelRoom[3])
 
-        file.write(str(tostring(root)))
+        file = open('Hotel.xml', 'w')
+        Answer = str(tostring(root))[2:]
+        Answer = Answer[:(len(Answer)-1)]
+        print(Answer)
+
+        file.write(Answer)
         file.close()
 
 
